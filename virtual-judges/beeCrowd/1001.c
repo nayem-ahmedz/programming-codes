4afa82e6069107dbed84ba6f1bfa1a93bad51c60
Nayem Ahmed <144725995+nayem-ahmedz@users.noreply.github.com>
2024-02-19 20:09:29 +0600

    Update 1001.c

--- virtual-judges/beeCrowd/1001.c
+++ virtual-judges/beeCrowd/1001.c
@@ -1,1 +1,11 @@
-hi
+//sum of two integers from input
+#include <stdio.h>
+ 
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int sum = a+b;
+    printf("X = %d\n", sum);
+ 
+    return 0;
+}
