0e327b54610813f91fecf098f71515e882833ac7
Nayem Ahmed <144725995+nayem-ahmedz@users.noreply.github.com>
2024-02-21 12:35:17 +0600

    Create p-1007.c

---
+++ virtual-judges/beeCrowd/p-1007.c
@@ -0,0 +1,10 @@
+//Difference between 2 products
+#include <stdio.h> 
+int main() {
+ 
+    int a, b, c, d;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    printf("DIFERENCA = %d\n", (a*b)-(c*d));
+ 
+    return 0;
+}
