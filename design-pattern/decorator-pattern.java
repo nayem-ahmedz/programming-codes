d79b2e7cc7b9e87e9e4ef49a7c729e5ab912197d
Nayem Ahmed <144725995+nayem-ahmedz@users.noreply.github.com>
2024-11-23 22:21:58 +0600

    Added exaple by Burger

--- design-pattern/decorator-pattern.java
+++ design-pattern/decorator-pattern.java
@@ -56,3 +56,77 @@ public class Decorator_Pattern {
         System.out.println("Price : " + milkCoffe2.getCost());
     }
 }
+
+
+
+
+//Example By Burger
+/*
+
+package com.mycompany.decorator_pattern;
+/**
+ *
+ * @author nayem
+ */
+
+interface Burger{
+    String getDesc();
+    double getCost();
+}
+class PlainBurger implements Burger{
+    public String getDesc(){
+        return "Plain Burger";
+    }
+    public double getCost(){
+        return 100;
+    }
+}
+class Decorator implements Burger{
+    protected Burger decoratedBurger;
+    public Decorator(Burger decoratedBurger) {
+        this.decoratedBurger = decoratedBurger;
+    }
+    public String getDesc(){
+        return decoratedBurger.getDesc();
+    }
+    public double getCost(){
+        return decoratedBurger.getCost();
+    }
+}
+class ChickenBurger extends Decorator{
+    public ChickenBurger(Burger decoratedBurger) {
+        super(decoratedBurger);
+    }
+    public String getDesc(){
+        return decoratedBurger.getDesc() + "(Chicken)";
+    }
+    public double getCost(){
+        return decoratedBurger.getCost() + 100;
+    }
+}
+class BeefBurger extends Decorator{
+    public BeefBurger(Burger decoratedBurger) {
+        super(decoratedBurger);
+    }
+    public String getDesc(){
+        return decoratedBurger.getDesc() + "(Beef)";
+    }
+    public double getCost(){
+        return decoratedBurger.getCost() + 200;
+    }
+}
+
+public class Decorator_Pattern {
+    
+    public static void main(String[] args) {
+        Burger normalBurger = new PlainBurger();
+        System.out.println("Description : " + normalBurger.getDesc());
+        System.out.println("Price : " + normalBurger.getCost());
+        
+        Burger beefBurger = new BeefBurger(new PlainBurger());
+        System.out.println("Description : " + beefBurger.getDesc());
+        System.out.println("Price : " + beefBurger.getCost());
+    }
+}
+
+*/
